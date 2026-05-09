12eee6cf69df42217e4abcf13cdf5a4017e481dd
tahmidhossaindev <tahmidhossain345@gmail.com>
2026-05-09 14:28:01 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,50 @@
+alert("hello")
+console.log("connection test");
+
+var name = "tahmid"
+var name = "tahmid hossain"
+console.log(name);
+
+let nam = "rafiur"
+console.log(nam);
+
+const eulersnumber = 2.718
+console.log(eulersnumber);
+
+
+let firstnumber = 50
+let secondnumber = 10
+
+console.log(firstnumber + secondnumber);
+console.log(firstnumber * secondnumber);
+console.log(firstnumber / secondnumber);
+
+
+let myName = "Tahmid Hossain"
+let myAge = 16
+
+console.log("My name is " + myName + "," + "My age is " + myAge);
+
+
+let boolean = true
+console.log(typeof boolean);
+
+let number = 67
+console.log(typeof number);
+
+let text = "hello"
+console.log(typeof text);
+
+
+
+{
+    var public = 1234
+}
+console.log(public);
+
+
+{
+    let private = 1234
+}
+console.log(private);
+
